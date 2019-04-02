4422d05d4b710fe195acdba70e0d63e990d8ecd5
saravanakumar.v <saravanakumar.velayutham@mavs.uta.edu>
2019-04-02 09:35:27 +0530

    refine output

--- Problem5_Southeros/TestCaseForSampleInputs.py
+++ Problem5_Southeros/TestCaseForSampleInputs.py
@@ -3,48 +3,40 @@ from Solution import SoutherosSolution
 
 class Tests:
     def RunProblem1(self):
-        sampleIo = """Who is the ruler of Southeros?
-                Ouput: None
-                Allies of Ruler?
-                Output: None
-                Input Messages to kingdoms from King Shan: 
-                Input: Air, "oaaawaala"
+        print('\nProblem1_Sample1')
+        print('Input Messages to kingdoms from King Shan:')
+        sampleIo = """Input: Air, "oaaawaala"
                 Input: Land, "a1d22n333a4444p"
                 Input: Ice, "zmzmzmzaztzozh\""""
+        print(sampleIo)
         southeros = SoutherosSolution()
-        print('Problem1_Sample1')
         southeros.ExecuteProblem1(sampleIo)
 
-        sampleIo = """Who is the ruler of Southeros?
-        Output: None
-        Allies of King Shan?
-        Output: None
-        Input Messages to kingdoms from King Shan:
-        Input: Air, "Let's swing the sword together"
+
+        print('\nProblem1_Sample2')
+        print('Input Messages to kingdoms from King Shan:')
+        sampleIo = """Input: Air, "Let's swing the sword together"
         Input: Land, "Die or play the tame of thrones"
         Input: Ice, "Ahoy! Fight for me with men and money"
         Input: Water, "Summer is coming"
         Input: Fire, "Drag on Martin!\""""
-        print('\nProblem1_Sample2')
+        print(sampleIo)
         southeros.ExecuteProblem1(sampleIo)
 
     def RunProblem2(self):
-        sampleIo = """Who is the ruler of Southeros?
-        Output: None
-        Allies of Ruler?
-        Output: None
-        Enter the kingdoms competing to be the ruler: 
-        Input: Land Air"""
-        #southeros = SoutherosSolution()
-        #southeros.ExecuteProblem2(sampleIo)
+        print('\nProblem2_Sample1')
+        print('Enter the kingdoms competing to be the ruler:')
+        sampleIo = """Input: Land Air"""
+        print(sampleIo)
+        southeros = SoutherosSolution()
+        southeros.ExecuteProblem2(sampleIo)
 
-        sampleIo = """Who is the ruler of Southeros? Output: None
-        Allies of Ruler?
-        Output: None
-        Enter the kingdoms competing to be the ruler: 
-        Input: Fire Space"""
+        print('\nProbelm2_Sample2')
+        print('Enter the kingdoms competing to be the ruler:')
+        print(sampleIo)
+        sampleIo = """Input: Fire Space"""
         southeros = SoutherosSolution()
         southeros.ExecuteProblem2(sampleIo)
 test = Tests()
-#test.RunProblem1()
+test.RunProblem1()
 test.RunProblem2()
